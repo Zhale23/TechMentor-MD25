c29e03776c677bb0c272c9e242ef8a89ba712e77
Zhale23 <zharicklondos@gmail.com>
2025-11-19 18:26:08 -0500

    feat: configuración swagger

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,21 @@ async function bootstrap() {
     .build(); 
 
   const document = SwaggerModule.createDocument(app, configDoc);
-  SwaggerModule.setup('api/docs', app, document);
+
+  const customOptions = {
+    swaggerOptions: {
+      url: '/api-json',
+    },
+    customCssUrl:[
+      'https://unpkg.com/swagger-ui-dist5.10.3/swagger-ui.css',
+    ],
+    customJs:[
+      'https://unpkg.com/swagger-ui-dist5.10.3/swagger-ui-bundle.js',
+      'https://unpkg.com/swagger-ui-dist5.10.3/swagger-ui-standalone-preset.js',
+    ]
+  }
+
+  SwaggerModule.setup('api/docs', app, document, customOptions);
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
